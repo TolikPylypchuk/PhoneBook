3e6e3cf4432958e2563de2f02604ab99d9ff00d1
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2016-10-16 19:07:41 +0300

    Added turning the page functionality

--- PhoneBook/UI/MainWindow.xaml.cs
+++ PhoneBook/UI/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ namespace PhoneBook.UI
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		#region Fields
+
 		private PageInfo peoplePageInfo = new PageInfo
 		{
 			EntriesPerPage = 10
@@ -23,10 +25,14 @@ namespace PhoneBook.UI
 			EntriesPerPage = 10
 		};
 
+		#endregion
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
 		}
+
+		#region Event handlers
 
 		private async void Window_Loaded(object sender, RoutedEventArgs e)
 		{
@@ -70,6 +76,40 @@ namespace PhoneBook.UI
 			}
 		}
 
+		private void previousPage_Click(
+			object sender,
+			RoutedEventArgs e)
+		{
+			if (this.entriesTabControl.SelectedItem ==
+				this.peopleTabItem)
+			{
+				this.peoplePageInfo.CurrentPage--;
+			} else if (this.entriesTabControl.SelectedItem ==
+				this.companiesTabItem)
+			{
+				this.companiesPageInfo.CurrentPage--;
+			}
+		}
+
+		private void nextPage_Click(
+			object sender,
+			RoutedEventArgs e)
+		{
+			if (this.entriesTabControl.SelectedItem ==
+				this.peopleTabItem)
+			{
+				this.peoplePageInfo.CurrentPage++;
+			} else if (this.entriesTabControl.SelectedItem ==
+				this.companiesTabItem)
+			{
+				this.companiesPageInfo.CurrentPage++;
+			}
+		}
+
+		#endregion
+
+		#region Other methods
+
 		private void LoadPeople(
 			IRepository<User> repo,
 			PageInfo info)
@@ -91,5 +131,7 @@ namespace PhoneBook.UI
 				.Take(info.EntriesPerPage)
 				.LoadAsync();
 		}
+
+		#endregion
 	}
 }
