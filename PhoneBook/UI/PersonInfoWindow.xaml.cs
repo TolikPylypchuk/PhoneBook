49eb649d5bcab1c1bae5e747be3a3e64abce2121
NataliaSlobodianiuk <slo0nata@gmail.com>
2016-11-15 23:43:10 +0200

    renaming window

---
+++ PhoneBook/UI/PersonInfoWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PhoneBook.UI
+{
+	/// <summary>
+	/// Interaction logic for PersonalInfoWindow.xaml
+	/// </summary>
+	public partial class PersonalInfoWindow : Window
+	{
+		public PersonalInfoWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
